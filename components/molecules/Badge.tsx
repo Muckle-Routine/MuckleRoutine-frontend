ff8b94706a8457026c6ba05a02c28f41879475e2
hx2ryu <hx2ryu@gmail.com>
2022-11-25 23:24:21 +0900

    refactor: modify style for bg-color

--- components/molecules/Badge.tsx
+++ components/molecules/Badge.tsx
@@ -6,7 +6,9 @@ type Props = {
 const Badge = ({ text, style }: Props) => {
     return (
         <div
-            className={`rounded-lg justify-center items-center flex py-1 px-[10px] mx-1 max-h-8 min-w-max bg-deactive ${style}`}
+            className={`rounded-lg justify-center items-center flex py-1 px-[10px] mx-1 max-h-8 min-w-max ${
+                style ? style : 'bg-deactive'
+            }`}
         >
             <span className="text-xs text-[10px] font-bold text-white">{text}</span>
         </div>
