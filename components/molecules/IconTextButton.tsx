6f3d38b5996e826e9ceb90794b298870e9255868
hx2ryu <hx2ryu@gmail.com>
2022-11-25 23:26:43 +0900

    refactor: remove useless style

--- components/molecules/IconTextButton.tsx
+++ components/molecules/IconTextButton.tsx
@@ -8,7 +8,7 @@ type Props = {
 const IconTextButton = ({ iconSrc, text, onClick, style }: Props) => {
     return (
         <button className={`flex min-w-full flex-row p-4 rounded-xl bg-background02 ${style}`} onClick={onClick}>
-            <img src={iconSrc} className="w-6 h-6" />
+            <img src={iconSrc} />
             <span className="text-white text-base font-bold ml-4">{text}</span>
         </button>
     );
