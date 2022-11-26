471404b3c9d59c3f974904104ce53e738f841c7f
hx2ryu <hx2ryu@gmail.com>
2022-11-26 21:57:45 +0900

    feat: wrap root with react-query provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import Head from 'next/head';
 import BottomSheetProvider from '@/components/organisms/BottomSheetProvider';
 import { Toaster } from 'react-hot-toast';
 import '../styles/globals.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
@@ -15,6 +16,8 @@ type AppPropsWithLayout = AppProps & {
     Component: NextPageWithLayout;
 };
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
     const getLayout = Component.getLayout ?? ((page) => page);
 
@@ -24,8 +27,10 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
                 <meta name="viewport" content="user-scalable=no,width=device-width,initial-scale=1,maximum-scale=1" />
             </Head>
             <BottomSheetProvider>
-                <Component {...pageProps} />
-                <Toaster />
+                <QueryClientProvider client={queryClient}>
+                    <Component {...pageProps} />
+                    <Toaster />
+                </QueryClientProvider>
             </BottomSheetProvider>
         </>,
     );
