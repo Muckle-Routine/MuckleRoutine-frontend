ab9250263c285c9d778d9190d56049d1b1bf4390
hx2ryu <hx2ryu@gmail.com>
2022-11-26 21:48:24 +0900

    feat: define chche keys for react-query

---
+++ services/statics.ts
@@ -0,0 +1,7 @@
+export const QUERY_KEY = {
+    getAllRoutine: '@get-all-routine',
+    getRoutinesPerCategory: '@get-routines-category',
+    getRoutineDetail: '@get-routine-detail',
+    getCertificationVote: '@get-certification-vote',
+    getCertification: '@get-certification',
+};
