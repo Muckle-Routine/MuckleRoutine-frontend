58b6406e8cfa095cad8b8e2d6f358be860398ce6
hx2ryu <hx2ryu@gmail.com>
2022-11-26 18:30:27 +0900

    feat: add util functions to use ipfs storage

--- utils/ipfs.ts
+++ utils/ipfs.ts
@@ -1,14 +1,22 @@
 import fleekStorage from '@fleekhq/fleek-storage-js';
 
+const API_KEY = process.env.NEXT_PUBLIC_IFPS_STORAGE_API_KEY || '';
+const SECRET_KEY = process.env.NEXT_PUBLIC_IFPS_STORAGE_API_SECRET_KEY || '';
+
 export async function getImageFromIpfs(key: string) {
     return await fleekStorage.get({
-        apiKey: process.env.NEXT_PUBLIC_IFPS_STORAGE_API_KEY || '',
-        apiSecret: process.env.NEXT_PUBLIC_IFPS_STORAGE_API_SECRET_KEY || '',
+        apiKey: API_KEY,
+        apiSecret: SECRET_KEY,
         key,
         getOptions: ['data', 'bucket', 'key', 'hash', 'publicUrl'],
     });
 }
 
-export async function uploadImage(filePath: string) {
-    //  fleekStorage.
+export async function uploadImage(data: string, key: string) {
+    await fleekStorage.upload({
+        apiKey: API_KEY,
+        apiSecret: SECRET_KEY,
+        key,
+        data,
+    });
 }
